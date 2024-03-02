d6c100b5b842c50c512b0ce44a33c8acb8e07c4f
Vlad Aheiev <vageev06@gmail.com>
2024-03-02 16:34:01 -0500

    added main search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container, TextField, Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Container
+        sx={{
+          display: "flex",
+        }}
+        maxWidth="md"
+      >
+        <TextField fullWidth label="Filled" variant="filled" />
+        <Button variant="contained" sx={{ textTransform: "capitalize" }}>
+          Search
+        </Button>
+      </Container>
+    </Container>
   );
 }
 
